8052db3d1d7ada5430c5ff0b4b4c95604934fb94
Eleee28 <144879688+Eleee28@users.noreply.github.com>
2023-09-13 15:44:02 +0000

    memchr and strncmp

---
+++ libft/ft_memchr.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+    int i;
+    char *ptr;
+    char *str;
+
+    str = s;
+    i = 0;
+    while (i < n && str[i])
+    {
+        if (str[i] == )
+    }
+}
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -17,4 +17,18 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (ret_value);
 }
+/*
+#include <string.h>
 
+int main(void)
+{
+	char str1[] = "ab", str2[] = "a\t";
+	size_t size = 2;
+	printf("%d\n", ft_strncmp(str1, str2, size));
+
+	char str3[] = "ab", str4[] = "a\t";
+	size_t size2 = 2;
+	printf("%d\n", strncmp(str3, str4, size2));
+	return (0);
+}
+*/
