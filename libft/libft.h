37a6e6b0505b7447f5153cde02edc11d7154b7bf
Eleee28 <144879688+Eleee28@users.noreply.github.com>
2023-09-13 14:18:32 +0000

    finished header

--- libft/libft.h
+++ libft/libft.h
@@ -1,7 +1,8 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
-typedef unsigned long long size_t;
+#include <stdio.h>
+#include <stdlib.h>
 
 int ft_isalpha(int c);
 int ft_isdigit(int c);
@@ -22,6 +23,9 @@ char    *ft_strrchr(const char *s, int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-
+char    *ft_strnstr(const char *str, const char *to_find, size_t len);
+int ft_atoi(const char *nptr);
+void    *ft_calloc(size_t nmemb, size_t size);
+char    *ft_strdup(const char *s);
 
 #endif
