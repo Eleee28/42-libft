91bbbc0255693b787ea13ac07351926f93f7a0e6
Eleee28 <144879688+Eleee28@users.noreply.github.com>
2023-09-13 16:07:19 +0000

    creation of Makefile

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,13 +3,32 @@
 void    *ft_memchr(const void *s, int c, size_t n)
 {
     int i;
-    char *ptr;
     char *str;
-
-    str = s;
+    
+    str = (char *)s;
     i = 0;
-    while (i < n && str[i])
-    {
-        if (str[i] == )
-    }
+    while (*(str + i) != (char)c && i < n)
+        {
+            if (!*(str + i))
+                return (0);
+            else
+                i++;
+        }
+        return (str + i);   
 }
+/*
+#include <string.h>
+
+int main()
+{
+    char *ptr, str[] = "Holaa", *ptr1;
+
+    ptr1 = memchr(str, 'l', 4);
+    printf("%s\n", ptr1);
+    ptr = ft_memchr(str, 'l', 4);
+    printf("%s\n", ptr);
+    return (0);
+}
+
+// Run tests to check that it works
+*/
