12a477d53841e5b9eae853e4e2c952bd28a1a142
Eleee28 <144879688+Eleee28@users.noreply.github.com>
2023-09-13 09:51:33 +0000

    new files

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void *ft_memcpy(void *dest, const void *src, unsigned long n)
+{
+    unsigned long i;
+    const char *aux_src;
+    char *aux_dest;
+
+    aux_src = src;
+    aux_dest = dest;
+
+    i = 0;
+    while (i < n && aux_src[i])
+    {
+        aux_dest[i] = aux_src[i];
+        i++;
+    }
+    return (dest);
+}
+
+int main(void)
+{
+    char src[] = "Hello World!", dest[] = "Hola Mundo!", *ptr;
+    printf("Src: %s\n", src);
+    printf("Original dest: %s\n", dest);
+    ptr = (char *)ft_memcpy(dest, src, 7);
+    printf("Modified dest: %s\n", ptr);
+    return (0);
+}
