3a047ad8eaa2614b03a575a705dcd934f654a851
Eleee28 <144879688+Eleee28@users.noreply.github.com>
2023-09-20 09:15:49 +0000

    strmapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 08:45:09 by codespace         #+#    #+#             */
+/*   Updated: 2023/09/20 09:15:00 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *str;
+    int     i;
+
+    i = 0;
+    while (s[i])        
+        i++;
+    str = malloc(i);
+    if (!str)
+        return (0);
+    i = 0;
+    while (s[i])
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = 0;
+    return (str);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 09:59:49 by ejuarros          #+#    #+#             */
-/*   Updated: 2023/09/19 09:49:03 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/20 08:50:40 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
 char    **ft_split(char const *s, char c);
 char    *ft_itoa(int n);
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
 
 #endif
