b0886e72c1af413ded528b2d54ebac3ca2182e4d
Eleee28 <144879688+Eleee28@users.noreply.github.com>
2023-09-20 07:51:22 +0000

    itoa done

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,25 +1,59 @@
 #include "libft.h"
-#include <stdio.h>
 
-char    *ft_itoa(int n)
+static int count_digits(int n)
 {
     int digits;
     int aux;
 
     digits = 0;
     aux = n;
-    while (aux != 0)    // count number of digits to do malloc
+    while (aux != 0)
     {
         digits++;
         aux /= 10;
     }
     if (n < 0)
         digits++;
-    printf("%d\n", digits);
-    return (0);
+    return (digits);
 }
+
+static int  ft_abs(int n)
+{
+    if (n < 0)
+        n *= -1;
+    return (n);
+}
+
+char    *ft_itoa(int n)
+{
+    char    *str;
+    int     i;
+    int     len;
+    int     end;
+
+    len = count_digits(n);
+    str = malloc(len);
+    i = 0;
+    if (n < 0)
+    {
+        str[i] = '-';
+        i++;
+    }
+    end = len - 1;
+    while (end >= i)
+    {
+        str[end] = (char){(ft_abs(n % 10)) + '0'};
+        n /= 10;
+        end--;
+    }
+    return (str);
+}
+/*
+#include <stdio.h>
 
 int main(void)
 {
-    ft_itoa(-12);
+    printf("%s\n", ft_itoa(2147483647));
+    return (0);
 }
+*/
