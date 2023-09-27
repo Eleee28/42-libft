607cf8b7ef8340522b1030a5f4aba676a399122b
Elena Juarros <ejuarros@c2r11s2.42madrid.com>
2023-09-27 08:43:37 +0200

    everything ok

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ejuarros <ejuarros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 11:52:07 by ejuarros          #+#    #+#             */
-/*   Updated: 2023/09/26 12:10:13 by ejuarros         ###   ########.fr       */
+/*   Updated: 2023/09/27 08:38:13 by ejuarros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	int		len;
 	t_list	*new;
+	t_list	*aux1;
+	void	*cont;
 
 	if (!lst || !*f || !del)
 		return (0);
-	len = ft_lstsize(lst);
+	new = 0;
 	while (lst)
 	{
-		new = ft_lstnew(f(lst->content));
-		if (!new)
+		cont = f(lst->content);
+		aux1 = ft_lstnew(cont);
+		if (!aux1)
 		{
-			ft_lstdelone(new, del);
+			del(cont);
+			ft_lstclear(&new, del);
 			return (0);
 		}
+		ft_lstadd_back(&new, aux1);
 		lst = lst->next;
 	}
 	return (new);
